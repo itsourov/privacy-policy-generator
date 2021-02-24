4f75785597e37bdf7a7ad420062634991bd3b02f
itsourov <79559219+itsourov@users.noreply.github.com>
2021-02-24 10:41:32 +0600

    index.php

---
+++ index.php
@@ -0,0 +1,29 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+<meta name="viewport" content="width=device-width, initial-scale=1">
+<link rel='stylesheet' href='https://cdnjs.cloudflare.com/ajax/libs/twitter-bootstrap/4.1.3/css/bootstrap.min.css'>
+<style>body{background:}.container{border-radius:5px;padding:20px;border-image:linear-gradient(100deg,#BF2727,#43cea2);border-top:25px solid rgba(214,238,198.5);box-shadow:0 0 50px}#T1{width:100%;padding:12px;border:1px solid #ccc;border-radius:4px;box-sizing:border-box;margin-top:6px;margin-bottom:16px;resize:vertical;outline:none}#T2{width:100%;padding:12px;border:1px solid #ccc;border-radius:4px;box-sizing:border-box;margin-top:6px;margin-bottom:16px;resize:vertical;outline:none}#T3{width:100%;padding:12px;border:1px solid #ccc;border-radius:4px;box-sizing:border-box;margin-top:6px;margin-bottom:16px;resize:vertical;outline:none}</style>
+</head>
+<body>
+<div class="container">
+<form name="gen" method="post" action="https://kitbox.org/test">
+<center><h2><span style="color: yellow;">GDPR Privacy Policy Generator By KitBox.org</h2></center>
+<label><span style="color: yellow;">Website Name</label>
+<br>
+<input  type="text" name="myname" id="T1" placeholder="Website Name"><br>
+<label><span style="color: yellow;">Website Url</label>
+<br>
+<input type="text" id="T2" name="myurl" placeholder="Website Url"><br>
+<label><span style="color: yellow;">Emali Id</label>
+<br>
+<input type="email" id="T3" placeholder="Emali Id" name="mymail">
+<br>
+<center>
+<p><button class="btn btn-outline-primary btn-lg" type="submit" >Generate</button></p>
+</center>
+
+</form>
+
+</body>
+</html>
